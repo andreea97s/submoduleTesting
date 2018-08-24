d96c68e70c7e453a2d956b80895dfd05c6005c52
Andreea <andreea.stegerean@x2mobile.net>
2018-08-24 12:12:49 +0300

    3 commit

--- SecondApp/WelcomeViewController.swift
+++ SecondApp/WelcomeViewController.swift
@@ -13,13 +13,7 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("LALALA")
+        print("HAHAH")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
