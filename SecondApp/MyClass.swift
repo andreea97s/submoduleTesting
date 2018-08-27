6a08fc0a708403cd0f5cdf189d16e3946ea85e58
Andreea <andreea.stegerean@x2mobile.net>
2018-08-27 11:06:07 +0300

    added a class

---
+++ SecondApp/MyClass.swift
@@ -0,0 +1,16 @@
+//
+//  MyClass.swift
+//  SecondApp
+//
+//  Created by Andreea Stegerean on 27/08/2018.
+//  Copyright © 2018 andreea. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    
+    static func doIt() {
+        print("DONE")
+    }
+}
